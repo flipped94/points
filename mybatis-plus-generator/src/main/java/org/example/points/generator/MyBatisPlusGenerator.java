f20507655499efbf236e0b630185014aaec2ca08
flipped <1123630646@qq.com>
2023-10-16 23:40:22 +0800

    添加mybatis-plus生成器

---
+++ mybatis-plus-generator/src/main/java/org/example/points/generator/MyBatisPlusGenerator.java
@@ -0,0 +1,46 @@
+package org.example.points.generator;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.sql.Types;
+import java.util.Collections;
+
+public class MyBatisPlusGenerator {
+
+    public static void main(String[] args) {
+        FastAutoGenerator.create(
+                        "jdbc:mysql://192.168.137.133:3306/points?characterEncoding=utf-8",
+                        "root",
+                        "123456")
+                .globalConfig(builder -> {
+                    builder.author("flipped") // 设置作者
+                            .fileOverride() // 覆盖已生成文件
+                            .disableOpenDir()
+                            .outputDir("D:\\workspace\\java\\points\\mybatis-generator\\src\\main\\java"); // 指定输出目录
+                })
+                .dataSourceConfig(builder -> builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
+                    int typeCode = metaInfo.getJdbcType().TYPE_CODE;
+                    if (typeCode == Types.SMALLINT) {
+                        // 自定义类型转换
+                        return DbColumnType.INTEGER;
+                    }
+                    return typeRegistry.getColumnType(metaInfo);
+
+                }))
+                .packageConfig(builder -> {
+                    builder.parent("org.example.generator.users") // 设置父包名
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D:\\workspace\\java\\points\\mybatis-generator\\src\\main\\resources\\mapper")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.entityBuilder().enableLombok();
+                    builder.controllerBuilder().enableRestStyle();
+                    builder.addInclude("t_users") // 设置需要生成的表名
+                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+}
