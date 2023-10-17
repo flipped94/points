dd48508cf0754ea869378a1ba0c9714b2e2bc40a
flipped <1123630646@qq.com>
2023-10-17 11:13:42 +0800

    删除方法

--- ms-service/ms-article-service/src/main/java/org/example/points/article/mapper/ArticleMapper.java
+++ ms-service/ms-article-service/src/main/java/org/example/points/article/mapper/ArticleMapper.java
@@ -1,13 +1,7 @@
 package org.example.points.article.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import org.apache.ibatis.annotations.Param;
-import org.example.points.article.PortalArticleQueryParam;
-import org.example.points.article.ArticleVO;
 import org.example.points.article.entity.Article;
-
-import java.util.List;
 
 /**
  * <p>
@@ -18,7 +12,5 @@ import java.util.List;
  * @since 2023-10-16
  */
 public interface ArticleMapper extends BaseMapper<Article> {
-
-    List<ArticleVO> selectPageVo(IPage<ArticleVO> page, @Param("param") PortalArticleQueryParam param);
 
 }
